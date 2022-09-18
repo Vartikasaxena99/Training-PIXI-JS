a7efa415f3dd882c0ed8f6c2f3dfb013b3ebafe7
Vartikasaxena99 <vartika.saxena_cs19@gla.ac.in>
2022-09-18 06:01:23 +0530

    done

--- Assignment2-CardDisplay/index.js
+++ Assignment2-CardDisplay/index.js
@@ -1,23 +1,61 @@
-import {Application } from "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/6.5.2/browser/pixi.mjs";
-import { start } from "./js/game.js";
-import { loadAssets } from "./js/loader.js";
-
-export function createGame(div) {
-    const game = new Application({
-        width:innerWidth,
-        height:innerHeight,
-        backgroundColor:0xabbde3
-    });
-    console.log('game', game);
-    div.appendChild(game.view);
-    return game;
-}
-
-const game = createGame(document.getElementById('game'));
-loadAssets(game,[
-    { name: "back", url: "assets/cardback.png" },
-    { name: "front", url: "assets/smilies.jpg" },
-  ], () => {
-    console.log('starting game');
-    start(game);
+const game = new PIXI.Application({
+    width: innerWidth,
+    height: innerHeight,
+    backgroundColor: 0xabdbe3,
   });
+  
+  document.getElementById("game").append(game.view);
+  
+  loadAssets(
+    [
+    
+      { name: "back", url: "assets/cardback1.jpeg" },
+    //  { name: "front", url: "./assets/cardback.png" },
+    ],
+    start
+  );
+  
+  // -------------------------------
+  const pBar = document.getElementById("bar");
+  const pText = document.getElementById("progress");
+  function preload(e) {
+    pBar.style.width = e.progress  + "%";
+    pText.innerText = e.progress + "%";
+    if (e.progress === 100) {
+      console.log("hide loader");
+      setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+      }, 500);
+    }
+    console.log(e.progress);
+  }
+  
+  function loadAssets(list, onLoadComplete) {
+    game.loader.onProgress.add(preload);
+    game.loader.add(list).load(onLoadComplete);
+  }
+  
+  function start(loader, resources) {
+    console.log("params ", arguments);
+  
+    
+  
+    var d = 180;
+    var s=0;
+    for (var i = 0; i < 2; i++) {
+      for(var j=0;j<6;j++){
+      const back = PIXI.Sprite.from(resources["back"].texture);
+      back.scale.set(0.12);
+      back.x = d;
+      back.y = 160+s;
+      game.stage.addChild(back);
+      d = d + 200;
+    }
+    s+=200; 
+    d=180;          
+  }
+
+    
+
+
+  }
